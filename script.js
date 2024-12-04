e098d742b924dbc5730b7e0354e44a001ee61521
Clément <collinclement@orange.fr>
2024-12-04 17:26:18 +0100

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1 : J'analyse les données
+// 2 : Je crée mon modèle de carte en Html
+// 3 : Je récupère la donnée et je l'affiche en consoloe
+// 4 : Je stock le chemin de la donnée dans la variable
+// 5 : Je crée les cartes remplie dynamiquement
+// 6 : Je les envois dans le dom
+// 7 : CA MARCHE PAS !!!
