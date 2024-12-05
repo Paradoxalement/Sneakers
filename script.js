0fbf99c2e1418a916f943619d062af86da7affae
Clément <collinclement@orange.fr>
2024-12-05 17:22:10 +0100

    upgrade

--- script.js
+++ script.js
@@ -16,14 +16,85 @@ fetch("sneakers.json")
     console.log(donnee);
 
     // Parcourt le tableau de donnee pour récupérer chaque objet (chaque recette)
-    donnee.forEach((recette) => {
-      // Appelle la fonction 'afficher' pour intégrer les cartes de chaque recette dans la page HTML
-      afficher(recette);
+    donnee.produits.forEach((chaussure) => {
+      // Appelle la fonction 'afficher' pour intégrer les cartes de chaque sneakers dans la page HTML
+      afficher(chaussure);
     });
+
+    donnee.avantagesClients.forEach(avantage => {
+        afficherAvantage(avantage)
+    });
+    
+    donnee.services.forEach(services => {
+        afficherservices(services)
+    });
+
+    donnee.temoignages.forEach(temoignages => {
+        affichertemoignages(temoignages)
+
+    })
+
+
+
+
+    let acrocheContainer = document.querySelector("#accroche")
+ acrocheContainer.innerHTML += donnee.texteAppelAction
   });
 
+function afficherAvantage(avantage) {
+    let avantagesContainer = document.querySelector("#avantagesC")
+       avantagesContainer.innerHTML+= `
+       
+       <div class="carteclient " >
+        <img src="assets/${avantage.image}" alt="" class=""> 
+        <p>${avantage.avantage}</p>
+        </div>
+        `
+}
 
- // ** Fonction : afficher **
-// Rôle : Insérer dynamiquement une carte de recette dans la page HTML avec des informations et un style spécifique
-// Paramètre : "recetteActuelle" - un objet contenant les informations d'une recette
-// Retour : Aucun (la fonction manipule directement le DOM)
+
+function afficher(prod) {
+    let avantagesContainer = document.querySelector("#produitsC")
+        avantagesContainer.innerHTML += `
+        
+         
+                <div class="carte">
+                    <img src="${prod.imageUrl}"
+                        alt="" class="w35">
+                    <div class="padding10">
+
+                        <h4>${prod.nom}</h4>
+                        <p>${prod.description}</p>
+                    </div>
+                </div>
+ `}
+
+ function afficherservices(service) {
+    let avantagesContainer = document.querySelector("#servicesS")
+        avantagesContainer.innerHTML += `
+        <div class="carteService flex spaceBetween"><a href=""></a>
+                    <img src="assets/${service.image}" alt="">
+                    <div class="padding10 alignCenter flex spaceBetween">
+                    <h3>${service.nom}</h3>
+                    <p>${service.description}</p>
+                      </div>  
+                </div>
+                `  
+                }
+    function affichertemoignages(temoignages) {
+        let avantagesContainer = document.querySelector("#temoignagesC")
+            avantagesContainer.innerHTML +=
+        `
+              <div>
+                    <div>
+                        <p>${temoignages.prenom}</p>
+                        <p>${temoignages.note}</p>
+                        </div>
+                        <p>${temoignages.typeExperience}</p>
+                    <p>${temoignages.commentaire}</p>
+                </div>
+                `
+                }
+
+        
+        
