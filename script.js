bfa2b46a371a48bca52580fcc91b969097eb7a1c
Clément <collinclement@orange.fr>
2024-12-05 15:26:42 +0100

    update

--- script.js
+++ script.js
@@ -5,3 +5,25 @@
 // 5 : Je crée les cartes remplie dynamiquement
 // 6 : Je les envois dans le dom
 // 7 : CA MARCHE PAS !!!
+
+fetch("sneakers.json")
+  .then((rep) => {
+    // Lorsque la réponse est reçue, elle est convertie en JSON pour être manipulée en tant qu'objet JavaScript
+    return rep.json();
+  })
+  .then((donnee) => {
+    // Une fois les données récupérées et converties, elles sont affichées dans la console pour vérification
+    console.log(donnee);
+
+    // Parcourt le tableau de donnee pour récupérer chaque objet (chaque recette)
+    donnee.forEach((recette) => {
+      // Appelle la fonction 'afficher' pour intégrer les cartes de chaque recette dans la page HTML
+      afficher(recette);
+    });
+  });
+
+
+ // ** Fonction : afficher **
+// Rôle : Insérer dynamiquement une carte de recette dans la page HTML avec des informations et un style spécifique
+// Paramètre : "recetteActuelle" - un objet contenant les informations d'une recette
+// Retour : Aucun (la fonction manipule directement le DOM)
